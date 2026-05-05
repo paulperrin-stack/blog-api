3e8a7c7f3857b5b173de8fd798ef23ac95c3869e
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-05-05 11:57:10 +0700

    chore(api): configure ESLint

---
+++ api/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.config.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: { ...globals.node },
+        },
+        rules: {
+            "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+            "no-console": "off",
+            eqeqeq: "error",
+        },
+    },
+    { ignores: ["node_modules/", "generated/", "dist/"] },
+];
